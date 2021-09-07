5335e1cb89ddb9a32d8def15714e29593d5b3dc9
cllcnkrt <cllcnkrt@outlook.com>
2021-09-07 11:19:09 +0300

    sidebarChat started

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,6 +5,8 @@ import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
+import SidebarChat from "./SidebarChat/SidebarChat";
 function Sidebar() {
   return (
     <div className="sidebar">
@@ -25,10 +27,21 @@ function Sidebar() {
       </div>
 
       <div className="sidebar__search">
-          
+          <div className="sidebar__searchContainer">
+            <SearchOutlined/>
+            <input type="text" placeholder="Search or start new chat"/>
+          </div>
       </div>
 
-      <div className="sidebar__chats"></div>
+      <div className="sidebar__chats">
+        <SidebarChat/>
+        <SidebarChat/>
+        <SidebarChat/>
+        <SidebarChat/>
+        <SidebarChat/>
+        <SidebarChat/>
+        <SidebarChat/>
+      </div>
     </div>
   );
 }
